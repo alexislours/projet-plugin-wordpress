fcc186e45c221528ed52b7da7bc5b37a001336a5
Alexis LOURS <alexislours@protonmail.com>
2021-07-28 23:07:16 +0200

    feat: plugin settings page

--- mapbox.php
+++ mapbox.php
@@ -12,6 +12,10 @@
  * Text Domain:       mapbox_iw
  */
 
+/* -------------------------------------------------------------------------- */
+/*                                Plugin setup                                */
+/* -------------------------------------------------------------------------- */
+
 register_activation_hook(__FILE__, 'mapbox_iw_install');
 
 function mapbox_iw_install()
@@ -43,3 +47,41 @@ function mapbox_iw_install()
       ) $charset_collate;";
   dbDelta($sqlData);
 }
+
+/* -------------------------------------------------------------------------- */
+/*                                Settings page                               */
+/* -------------------------------------------------------------------------- */
+
+function mapbox_iw_register_settings()
+{
+  add_option('mapbox_iw_token', 'Your Mapbox token');
+  register_setting('mapbox_iw_options_group', 'mapbox_iw_token', 'mapbox_iw_callback');
+}
+add_action('admin_init', 'mapbox_iw_register_settings');
+
+function mapbox_iw_register_options_page()
+{
+  add_options_page('Mapbox', 'Mapbox Menu', 'manage_options', 'mapbox_iw', 'mapbox_iw_options_page');
+}
+add_action('admin_menu', 'mapbox_iw_register_options_page');
+
+function mapbox_iw_options_page()
+{
+?>
+  <div>
+    <?php screen_icon(); ?>
+    <h2>Mapbox settings</h2>
+    <form method="post" action="options.php">
+      <?php settings_fields('mapbox_iw_options_group'); ?>
+      <p>Global settings for Mapbox</p>
+      <table>
+        <tr valign="top">
+          <th scope="row"><label for="mapbox_iw_token">Token</label></th>
+          <td><input type="text" id="mapbox_iw_token" name="mapbox_iw_token" value="<?php echo get_option('mapbox_iw_token'); ?>" /></td>
+        </tr>
+      </table>
+      <?php submit_button(); ?>
+    </form>
+  </div>
+<?php
+} ?>
--- uninstall.php
+++ uninstall.php
@@ -3,6 +3,9 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
+$option_name = 'mapbox_iw_token';
+delete_option($option_name);
+
 global $wpdb;
 $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}mapbox_iw_settings");
 $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}mapbox_iw_data");
